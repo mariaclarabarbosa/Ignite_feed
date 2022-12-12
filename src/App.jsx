d52f26632ec8c1e996a064957cb8ecf0df1255e4
mariaclarabarbosa <clara_estrella@protonmail.com>
2022-12-12 19:31:28 -0300

    chore: criação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './global.css';
+
+import { Header } from "./components/Header/Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello Word!</h1>
+    </div>
+  )
+}
+
+export default App
